9c47e22e775a633a894385b86e67433e5f38504d
devcraftmyit <54175060+devcraftmyit@users.noreply.github.com>
2020-11-03 09:46:06 +0000

    385: Forcibly Exiting a Program

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,9 @@
 import 'package:prompter_rt/prompter_rt.dart';
-
+import 'dart:io';
 void main() {
   final prompter = new Prompter();
-  prompter.askBinary('Are you here to convert an image');
+  final choice = prompter.askBinary('Are you here to convert an image');
+  if (!choice) {
+    exit(0);
+  }
 }
