f3c948ae7d11d295d165665c9d18b1880f44ca39
devcraftmyit <54175060+devcraftmyit@users.noreply.github.com>
2020-11-04 06:40:48 +0000

    386: Prompting for File Type

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,18 @@
 import 'package:prompter_rt/prompter_rt.dart';
 import 'dart:io';
+
 void main() {
   final prompter = new Prompter();
   final choice = prompter.askBinary('Are you here to convert an image');
   if (!choice) {
     exit(0);
   }
+  prompter.askMultiple('Select Format', buildFormatOptions());
+}
+
+List<Options> buildFormatOptions() {
+  return [
+    new Options('Convert to jpg', 'jpg'),
+    new Options('Convert to png', 'png')
+  ];
 }
