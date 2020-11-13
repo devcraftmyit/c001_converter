69c45beb56b28c9d2ef40ebd14d2e7b7ac9de90e
devcraftmyit <54175060+devcraftmyit@users.noreply.github.com>
2020-11-13 09:17:39 +0000

    395: Reading File Contents

--- lib/src/converter.dart
+++ lib/src/converter.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:image/image.dart';
 
-convertImage(FileSystemEntity selectedFile, String format){
-
+convertImage(FileSystemEntity selectedFile, String format) {
+  final rawImage = (selectedFile as File).readAsBytes();
+  final image = decodeImage(rawImage);
 }
