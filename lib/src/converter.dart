6c61282f35ad3373ac709abdf710f2c2f55e9db1
devcraftmyit <54175060+devcraftmyit@users.noreply.github.com>
2020-11-14 07:19:30 +0000

    396 Encoding to JPG or PNG

--- lib/src/converter.dart
+++ lib/src/converter.dart
@@ -4,4 +4,13 @@ import 'package:image/image.dart';
 convertImage(FileSystemEntity selectedFile, String format) {
   final rawImage = (selectedFile as File).readAsBytesSync();
   final image = decodeImage(rawImage);
+  var newImage; 
+  if (format == 'jpg') {
+    newImage = encodeJpg(image);
+  } else if (format == 'png') {
+    newImage = encodePng(image);
+  } else {
+    print('Unsupported File Type');
+  }
+  ;
 }
